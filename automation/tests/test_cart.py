efc42bb7b3db7909de3554b0d423bf2c27a7b9c6
Meenakshi Adiandra <meenakshiadiandra199905@gmail.com>
2026-01-09 14:19:29 +0530

    Added first Selenium automation script to open browser

--- automation/tests/test_cart.py
+++ automation/tests/test_cart.py
@@ -1,1 +1,13 @@
 # Automation tests for Shopping Cart
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def test_open_browser():
+  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+  driver.get("https://www.google.com")
+  time.sleep(5)
+  driver.quit()
+
+test_open_browser()
